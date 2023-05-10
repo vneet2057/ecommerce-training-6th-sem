eeb0b444f9473b522e36b7bf97cc7dfeeaf55f2a
vneet2057 <divin828@gmail.com>
2023-05-10 10:59:56 +0545

    push

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //admin user seeder
+        DB::table('users')->insert([
+            'name'=> 'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('password'),
+            'isRole'=>'admin'
+        ]);
+    }
+}
